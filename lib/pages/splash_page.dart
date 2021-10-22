9633cbd2e5c59c815d5761905260bdfd3a91b67c
Sudhindra3000 <sudhindra5678@gmail.com>
2021-10-22 18:44:00 +0530

    navigate to Home from Splash after delay

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () => Get.toNamed("/home"));
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
