97ab06f6c37affb6220090ac0673366f93cbb67d
Sudhindra3000 <sudhindra5678@gmail.com>
2021-10-22 15:47:39 +0530

    add basic layout to PlayPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:draw_royale/pages/home.dart';
-import 'package:draw_royale/pages/play.dart';
+import 'package:draw_royale/pages/play_page.dart';
 import 'package:draw_royale/pages/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        initialRoute: '/',
+        initialRoute: '/play',
         routes: {
           '/': (context) => const Splash(),
           '/home': (context) => const Home(),
-          '/play': (context) => const Play(),
+          '/play': (context) => const PlayPage(),
         },
       ),
     );
